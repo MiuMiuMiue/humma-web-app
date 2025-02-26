bd8370f3cbb5d142f7c89da3db79cefb9da50a7d
SparkFan626 <fanyixing0626@gmail.com>
2025-02-25 22:52:14 -0800

    get user profile info

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import register, login
+from .views import register, login, getUserProfile
 
 urlpatterns = [
     path('api/user/register', register, name='register'),
     path('api/user/login', login, name='login'),
+    path("api/user/profile", getUserProfile, name="getUserProfile"),
 ]
--- backend/user/views.py
+++ backend/user/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework_simplejwt.tokens import RefreshToken
-from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer
+from .serializers import UserRegisterSerializer, UserLoginSerializer, UserSerializer, UserProfileSerializer
 
 User = get_user_model()
 
@@ -34,3 +34,10 @@ def login(request):
             }
         })
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def getUserProfile(request):
+    user = request.user  
+    serializer = UserProfileSerializer(user)  
+    return Response(serializer.data, status=status.HTTP_200_OK)  
